67a93a9a0c4310539e55b0e6460ff1d4141f33f7
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-04-15 02:28:34 +0530

    Cosmos

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -24,7 +24,7 @@ export const login = asyncMidddlewareError(
         res.cookie("jwtoken", token, {
             expires: new Date(Date.now() + 128986400),
             httpOnly: false,
-            domain: "localhost:3000"
+            domain: "http://localhost:3000"
         });
 
         res.status(200).json({
