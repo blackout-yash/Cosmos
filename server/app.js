ae5fbcb0c6f1d4ce82288a9c3ffd3e4554a8808f
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-04-16 01:28:00 +0530

    Cosmos

--- server/app.js
+++ server/app.js
@@ -30,6 +30,7 @@ app.use(function (req, res, next) {
     else res.setHeader('Access-Control-Allow-Origin', "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
